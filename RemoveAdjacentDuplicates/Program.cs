c6e000b9b33c41c993a8451f9a718895c72c5036
chitraphp <chitra.atmakuru@gmail.com>
2018-09-23 18:48:25 -0700

    Add project files.

---
+++ RemoveAdjacentDuplicates/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RemoveAdjacentDuplicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
